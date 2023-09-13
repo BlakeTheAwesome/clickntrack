f6c5f5ef9d0ae6def23d32f291c97ba45024c37a
Blake Preston <blake.preston@hotmail.com>
2023-09-13 12:11:04 +1000

    Added vite-plugin-singlefile - to make it work when run as a local file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import { viteSingleFile } from "vite-plugin-singlefile"
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '', // This makes paths relative, so that the app can run without a server
   plugins: [
     vue(),
+    viteSingleFile(),
   ],
   resolve: {
     alias: {
