2e915b55e75a1d6b2463f56492b0c5a4f9c1eba0
Blake Preston <blake.preston@hotmail.com>
2024-08-12 21:19:03 +1000

    Add histoire to the project

---
+++ src/histoire.setup.ts
@@ -0,0 +1,13 @@
+// import './assets/main.css'
+import { installPrimeVue } from './plugins/primevue'
+import { installPinia } from './plugins/pinia'
+import { defineSetupVue3 } from '@histoire/plugin-vue'
+
+import './styles/main.css'
+import './styles/histoire.css'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const setupVue3 = defineSetupVue3(({ app, story, variant }) => {
+	installPinia(app)
+	installPrimeVue(app)
+  })
