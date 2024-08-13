80bee1bd50dec94517529a2e802ca020ad0397ae
Blake Preston <blake.preston@hotmail.com>
2024-08-13 11:42:58 +1000

    Add histoire stories for all non-page components

---
+++ src/components/Debug/SetLayoutStore.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useLayoutStore } from '@/stores/layoutStore'
+import { onBeforeMount, useAttrs } from 'vue'
+const layoutStore = useLayoutStore()
+const attrs = useAttrs()
+
+onBeforeMount(() => {
+  for (const [k, v] of Object.entries(attrs)) {
+    ;(layoutStore as any)[k] = v
+  }
+})
+</script>
+
+<template>
+  <div class="set-layout-store"></div>
+</template>
---
+++ src/components/Grid/TrackerGrid.story.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import StoryComponent from './TrackerGrid.vue'
+import { useTrackerStore } from '@/stores/trackerStore'
+import SetLayoutStore from '@/components/Debug/SetLayoutStore.vue'
+const trackerStore = useTrackerStore()
+</script>
+
+<template>
+  <Story>
+    <Variant title="Square Grid">
+      <SetLayoutStore itemShape="Square" />
+      <StoryComponent :gridItems="trackerStore.gridItems" :gridRowLen="10" :cellSize="48" layout="Grid" :filter="''" />
+    </Variant>
+    <Variant title="Hex Grid">
+      <SetLayoutStore itemShape="Hex" />
+      <StoryComponent :gridItems="trackerStore.gridItems" :gridRowLen="10" :cellSize="48" layout="Hex" :filter="''" />
+    </Variant>
+  </Story>
+</template>
