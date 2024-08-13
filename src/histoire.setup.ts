104f9f19a631ccc1abbe006aff771ee553626f3d
Blake Preston <blake.preston@hotmail.com>
2024-08-13 11:13:42 +1000

    Disable pinia persistence in Histoire

--- src/histoire.setup.ts
+++ src/histoire.setup.ts
@@ -8,6 +8,6 @@ import './styles/histoire.css'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const setupVue3 = defineSetupVue3(({ app, story, variant }) => {
-	installPinia(app)
-	installPrimeVue(app)
-  })
+  installPinia(app, false)
+  installPrimeVue(app)
+})
