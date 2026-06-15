e5962ff05abf9db9cc12c4d6ab60b9a19c3fd280
Blake Preston <blake.preston@hotmail.com>
2026-06-15 22:48:16 +1000

    Add global error handling, with option to clear local storage

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import ErrorBoundary from '@/components/ErrorBoundary.vue'
 </script>
 
 <template>
   <main>
-    <RouterView />
+    <ErrorBoundary>
+      <RouterView />
+    </ErrorBoundary>
   </main>
 </template>
